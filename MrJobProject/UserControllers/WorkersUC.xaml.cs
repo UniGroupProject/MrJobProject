acd2515f41c68f76a06bf1fc53b824182f9ebef1
hermitty <47337808+hermitty@users.noreply.github.com>
2019-04-09 18:09:31 +0300

    Worker list display

--- MrJobProject/UserControllers/WorkersUC.xaml.cs
+++ MrJobProject/UserControllers/WorkersUC.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MrJobProject.Data;
+using SQLite;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,81 @@ namespace MrJobProject.UserControllers
     /// </summary>
     public partial class WorkersUC : UserControl
     {
+        ObservableCollection<Worker> workers;
+
         public WorkersUC()
         {
             InitializeComponent();
+
+            workers = new ObservableCollection<Worker>();
+
+            ReadDatabase();
+
+            WorkersList.ItemsSource = workers;
+        }
+
+        private void ReadDatabase()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Worker>();
+                workers = new ObservableCollection<Worker>(connection.Table<Worker>().ToList().OrderByDescending(c => c.Id).ToList());
+            }
+        }
+
+        private void AddNewWorker(object sender, RoutedEventArgs e)
+        {
+            if (nameValue.Text != null)
+            {
+                Worker worker = new Worker()
+                {
+                    Name = nameValue.Text,
+                    Status = statusValue.IsChecked.GetValueOrDefault()
+                };
+                using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+                {
+                    connection.CreateTable<Worker>();
+                    connection.Insert(worker);
+                }
+                ReadDatabase();
+                WorkersList.ItemsSource = workers;
+                nameValue.Text = "";
+                statusValue.IsChecked = true;
+            }
+        }
+
+        private void DatacontextItem_Delete(object sender, RoutedEventArgs e) //right click
+        {
+            Worker worker = WorkersList.SelectedItem as Worker;
+            string question = "Are you sure you want to delete " + worker.Name; //language item
+            MessageBoxResult Result = System.Windows.MessageBox.Show(question, "", System.Windows.MessageBoxButton.OKCancel);
+
+            if (Result == MessageBoxResult.OK)
+            {
+                if (WorkersList.SelectedItem != null)
+                {
+                    using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+                    {
+                        connection.CreateTable<Worker>();
+                        connection.Delete(WorkersList.SelectedItem);
+                    }
+                    ReadDatabase();
+                    WorkersList.ItemsSource = workers;
+                }
+            }
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e) //enter
+        {
+            if (e.Key == Key.Return)
+            {
+                AddNewWorker(sender, null);
+            }
+        }
+
+        private void EditWorker(object sender, MouseButtonEventArgs e) //double click
+        {
+
         }
     }
 }
