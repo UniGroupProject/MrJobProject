372611caef8d9409877b79c8c7b96e3a731575ec
hermitty <47337808+hermitty@users.noreply.github.com>
2019-05-10 11:37:02 +0200

    Added new menu tab

--- MrJobProject/MainWindow.xaml.cs
+++ MrJobProject/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using MrJobProject.Data;
 using MrJobProject.UserControllers;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace MrJobProject
 {
@@ -9,22 +10,49 @@ namespace MrJobProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        HolidayDateUC holiday;
         public MainWindow()
         {
             InitializeComponent();
-            Holiday.Children.Add(new HolidayUC());
         }
 
         public void ChangeToHolidayDateUC(Worker worker)
         {
-            Holiday.Children.Clear();
-            Holiday.Children.Add(new HolidayDateUC(worker));
+            HolidayGrid.Visibility = Visibility.Hidden;
+            holiday = new HolidayDateUC(worker);
+            MainView.Children.Add(holiday);
+
         }
 
         public void BackToHolidayUC(Worker worker)
         {
-            Holiday.Children.Clear();
-            Holiday.Children.Add(new HolidayUC());
+            MainView.Children.Remove(holiday);
+            HolidayGrid.Visibility = Visibility.Visible;
+        }
+
+        private void MenuClick(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+            GridCursor.SetValue(Grid.ColumnProperty, index);
+            ScheduleGrid.Visibility = Visibility.Hidden;
+            WorkersGrid.Visibility = Visibility.Hidden;
+            HolidayGrid.Visibility = Visibility.Hidden;
+            ListsGrid.Visibility = Visibility.Hidden;
+            switch (index)
+            {
+                case 0:
+                    ScheduleGrid.Visibility = Visibility.Visible;
+                    break;
+                case 1:
+                    WorkersGrid.Visibility = Visibility.Visible;
+                    break;
+                case 2:
+                    HolidayGrid.Visibility = Visibility.Visible;
+                    break;
+                case 3:
+                    ListsGrid.Visibility = Visibility.Visible;
+                    break;
+            }
         }
     }
 }
